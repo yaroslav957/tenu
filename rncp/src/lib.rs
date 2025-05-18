12d82fe466c9a2d70525f7313219cdc37712983c
Leonid E. <am4ik1337@gmail.com>
2025-05-18 22:50:56 +0300

    Упростил апи (ценной нескольких аллокаций) ⚡️⚡️⚡️

--- rncp/src/lib.rs
+++ rncp/src/lib.rs
@@ -1,12 +1,2 @@
-#![no_std]
+pub use clap;
 
-extern crate alloc;
-
-pub mod builder;
-pub(crate) mod env;
-pub mod error;
-pub mod parser;
-
-pub mod prelude {
-    pub use crate::env::*;
-}
--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,29 +1,7 @@
-use core::ptr;
-use rncp::{prelude::RawArgs, *};
-use std::os::raw::c_char;
-
-unsafe extern "C" fn capture(argc: i32, argv: *const *const c_char) {
-    unsafe {
-        ARGC = argc;
-        ARGV = argv;
-    }
-}
-
-static mut ARGC: i32 = 0;
-static mut ARGV: *const *const c_char = ptr::null();
-
-#[cfg_attr(target_os = "macos", unsafe(link_section = "__DATA,__mod_init_func"))]
-#[cfg_attr(not(target_os = "macos"), unsafe(link_section = ".init_array"))]
-#[used]
-static CAPTURE: unsafe extern "C" fn(i32, *const *const c_char) = capture;
-
 fn main() {
-    let args = unsafe { RawArgs::from_raw_env(ARGC, ARGV).unwrap() };
-    let foo = builder::ArgBuilder::new("foo")
-        .with_value(&parser::IntParser)
-        .short()
-        .get(&args);
+    let args = std::env::args();
 
-    // Run with -- foo 52 as an arguments and see what's about to happen (segfault) \j
-    println!("{foo:?}");
+    for (n, arg) in args.enumerate() {
+        println!("[{n}] {arg}");
+    }
 }
