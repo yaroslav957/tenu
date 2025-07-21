358aa097ebf981c90b3b4553067cddd321a3003e
yaroslav957 <yaroslav.lobachev9@gmail.com>
2025-07-21 19:35:08 +0300

    feat(troll): main.rs comments

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -1,13 +1,2 @@
-use tenu::{
-    args,
-    lex::{ArgType, LookupTable, Parser},
-};
-
-const OPTIONS: LookupTable = LookupTable(&[
-    ("help", ArgType::None, 'h'),
-    ("output", ArgType::Required, 'o'),
-    ("list", ArgType::None, 'l'),
-    ("verbose", ArgType::Option, 'v'),
-]);
-
+//make new tests cuz new api-builder & parser rewrite
 fn main() {}
